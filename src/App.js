724780cac8747e797fb6b1e360ecb01b2a2ab57a
Stephen Smith <wearypossum4770@yahoo.com>
2021-07-27 22:52:12 -0400

    sync

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import Weather from "./component/Weather";
+import Home from "./component/Home";
+import Dashboard from "./component/Dashboard";
+import About from "./component/About";
 import "./App.css";
-function App() {
-  return <Weather />;
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  withRouter,
+} from "react-router-dom";
+export default function App() {
+  // prevProps if (props.location !== prevProps.location){onRouteChanged()}
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link onClick={() => console.log("HEY")} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/weather">Weather</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/weather" component={Weather} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
-export default App;
--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,66 +1,57 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { WeatherContext } from "../context/WeatherContext";
 import useFetchData from "../hooks/useFetchData";
-const user = {
-  first_name: "Rowan",
-
-  last_name: "Atkinson",
-};
-//   https://dev.to/nipu/tips-tricks-of-javascript-react-3ncc
-window.user = user;
-const dateBuilder = (d) => {
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`;
-};
-export default function Weather() {
-  const [query, setQuery] = useState("");
+export default function Weather({
+  api_key = "7af4edd80277ecd98c9eb7b15f9cfb84",
+  country_code = "US",
+  zip_code = 37416,
+  state_code = "Tennesse",
+  city_name = "Chattanooga",
+}) {
+  const [location, setLocation] = useState({ long: null, lat: null });
   const [weather, setWeather] = useState();
   let [isLoading, setIsLoading] = useState(true);
-  let api_key = "7af4edd80277ecd98c9eb7b15f9cfb84";
-  let country_code = "US";
-  let zip_code = 37416;
-  let state_code = "Tennesse";
-  let city_name = "Chattanooga";
+  let [hide, setHide] = useState(true);
   let { response, errors } = useFetchData(
-    `https://api.openweathermap.org/data/2.5/weather?zip=${zip_code},${country_code}&appid=${api_key}&units=imperial`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.long}&appid=${api_key}&units=imperial`
   );
+  const getLocation = async () => {
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let { longitude, latitude } = position.coords;
+        setLocation({ lat: latitude, long: longitude });
+      });
+    } catch (err) {
+      alert("Unable to retrieve your location");
+    }
+  };
   useEffect(() => {
     if (response && isLoading) {
       setWeather(response);
       setIsLoading(false);
+      setHide(false);
     }
-  }, []);
+    if (errors) {
+      setHide(false);
+      alert("Cannot obtain weather");
+      setIsLoading(false);
+    }
+  }, [isLoading, response, errors]);
   return (
     <div>
-      {!isLoading && (
+      {isLoading ? (
+        <div>
+          <button onClick={getLocation}>Get My Location</button>
+          {hide || <h3>Obtaining Location</h3>}
+        </div>
+      ) : (
         <div>
           <h1>{weather.name}</h1>
+          <image
+            alt={`weather icon shows ${weather?.weather[0]?.main}`}
+            src={`http://openweathermap.org/img/wn/${weather?.weather[0]?.icon}d@2x.png`}
+          />
+          <h1>{`${weather?.weather[0]?.icon}`}</h1>
           <h1>{`Air Temperature: ${weather.main.temp} °F`}</h1>
           <h1>{`Feels Like: ${weather.main.feels_like} °F`}</h1>
         </div>
